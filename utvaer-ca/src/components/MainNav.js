90ba3e25be4b2ff2627b8d7bc5e2d2a3d38f3fa1
Tiffany Utvaer Gasser <tiffany.u.gasser@gmail.com>
2021-11-16 13:13:43 +0100

    mainNav created&updated

--- utvaer-ca/src/components/MainNav.js
+++ utvaer-ca/src/components/MainNav.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
 function MainNav() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" expand="lg">
       <Container>
         <Navbar.Brand>The YAY Company</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink activeClassName={classes.active} to="/home">
-              Home
-            </NavLink>
-            <NavLink activeClassName={classes.active} to="/news">
-              News
-            </NavLink>
+            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/news">News</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
